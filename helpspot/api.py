84cd7ff137c80394b156874a080fe5c5e349a831
Greg Aker <greg@gregaker.net>
2013-04-21 12:13:17 -0500

    Adding tox and starting on Python 3 support.

--- helpspot/api.py
+++ helpspot/api.py
@@ -31,13 +31,32 @@ print hs.private_version()
 
 hs.private_request_update(xRequest='12345', Custom28='90210')
 """
+from __future__ import print_function
+import base64
 
-import urllib2
-from urllib import urlencode
+try:
+    import urllib2 as urllib
+except ImportError:
+    # python 3
+    import urllib
+
+try:
+    from urllib import urlencode
+except ImportError:
+    # python 3
+    from urllib.parse import urlencode
+
+try:
+    from urllib2 import HTTPHandler
+except ImportError:
+    # python 3
+    from urllib.request import HTTPHandler
+
 try:
     import json
 except ImportError:
     import simplejson as json
+
 
 _POST_METHODS = [
     'request.create',
@@ -52,13 +71,14 @@ _POST_METHODS = [
     'private.request.unsubscribe',
 ]
 
+
 class HelpSpotError(Exception):
     """
     Exception raised when HelpSpot returns HTTP status code 400.
     The instance varibles are as follows:
 
         err_mesg - Description of the Error from HelpSpot.
-        err_id - Error ID of the Error from HelpSpot. 
+        err_id - Error ID of the Error from HelpSpot.
 
     This does not handle the error case if either public or private API
     are not enabled.
@@ -70,10 +90,11 @@ class HelpSpotError(Exception):
 
     def __str__(self):
         return '%s (%s)' % (self.err_mesg, self.err_id)
-    
-class HelpSpotHandler(urllib2.HTTPHandler):
+
+
+class HelpSpotHandler(HTTPHandler):
     """
-    urllib2 opener class that handles (most) HelpSpot API errors.
+    urllib opener class that handles (most) HelpSpot API errors.
     HelpSpot returns HTTP status code 400 for (most) errors.
     """
     def http_error_400(self, req, fp, code, msg, hdrs):
@@ -87,7 +108,8 @@ class HelpSpotHandler(urllib2.HTTPHandler):
             err_id = 0
         raise HelpSpotError(err_mesg=err_mesg, err_id=err_id)
 
-class HelpSpotAPI:
+
+class HelpSpotAPI(object):
     """
     A HelpSpot API call. Will implicitly raise HelpSpotError
     on API errors otherwise it will return the unmarshalled
@@ -101,13 +123,13 @@ class HelpSpotAPI:
         self.user = user
         self.password = password
         s = '%s:%s' % (self.user, self.password)
-        self.authz = s.encode('base64').rstrip()
+        self.authz = base64.b64encode(s.encode('utf-8')).rstrip()
         self.uri = uri.rstrip('/') + '/api/index.php?method='
         if self.method in _POST_METHODS:
             self.action = 'POST'
         else:
             self.action = 'GET'
-        
+
     def __call__(self, **kwargs):
         """
         Calls the remote HelpSpot method on the HelpSpot server.
@@ -121,18 +143,30 @@ class HelpSpotAPI:
         else:
             message_body = params
         uri = '%s&output=json' % uri
-        req = urllib2.Request(uri)
+
+        try:
+            req = urllib.Request(uri)
+        except AttributeError:
+            # python 3
+            req = urllib.request.Request(uri)
         # Older Python versions may want to uncomment the following line
         # to prevent the server from keeping the connection open.
         #req.add_header('Connection', 'close')
         if self.method.startswith('private.'):
-            req.add_header('Authorization', 'Basic %s' % self.authz) 
-        # urllib2.urlopen could raise URLError, or HelpSpotError
-        r = urllib2.urlopen(req, message_body)
+            req.add_header('Authorization', 'Basic %s' % self.authz)
+
+        try:
+            # urllib.urlopen could raise URLError, or HelpSpotError
+            r = urllib.urlopen(req, message_body)
+        except AttributeError:
+            # python 3
+            r = urllib.request.urlopen(req, message_body)
+
         # XXX Detect errors when API not enabled
         return json.loads(r.read())
 
-class HelpSpot:
+
+class HelpSpot(object):
     """
     A wrapper object around the HelpSpotAPI. Any method call on this
     object will create and invoke a HelpSpotAPI object.
@@ -141,14 +175,19 @@ class HelpSpot:
         self.uri = uri
         self.user = user
         self.password = password
-        opener = urllib2.build_opener(HelpSpotHandler(debuglevel=debuglevel))
-        urllib2.install_opener(opener)
+        try:
+            opener = urllib.build_opener(HelpSpotHandler(debuglevel=debuglevel))
+            urllib.install_opener(opener)
+        except AttributeError:
+            opener = urllib.request.build_opener(HelpSpotHandler(debuglevel=debuglevel))
+            urllib.request.install_opener(opener)
 
     def __getattr__(self, key):
         try:
             return object.__getattr__(self, key)
         except AttributeError:
             return HelpSpotAPI(key, self.user, self.password, self.uri)
+
 
 def main():
     user = sys.argv[1]
@@ -157,10 +196,11 @@ def main():
     hs = HelpSpot(user=user, password=password, uri=uri)
 
     ver1 = hs.version()
-    print "version returned", ver1
+    print("version returned", ver1)
     ver2 = hs.private_version()
-    print "private.version returned", ver2
+    print("private.version returned", ver2)
     assert ver1 == ver2
+
 
 if __name__ == '__main__':
     import sys
--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(name = 'helpspot', version='1.0',
-        packages = ['helpspot'])
+import os
+from setuptools import setup, find_packages
+
+def long_description():
+    output = []
+    for filename in ('README.rst', ):
+        f = open(os.path.join(os.path.dirname(__file__), filename))
+        try:
+            output.append(f.read())
+        except FileNotFoundError:
+            pass
+        finally:
+            f.close()
+    return '\n\n'.join(output)
+
+
+setup(
+    name='helpspot',
+    version='1.0',
+    description='Python HelpSpot API Wrapper',
+    long_description=long_description(),
+    packages=['helpspot']
+)
 
